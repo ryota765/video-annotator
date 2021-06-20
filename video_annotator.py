76ced4e670172e51bef963f20294e06f521c1bbf
Ryota <ryota15765@icloud.com>
2021-06-20 18:28:02 +0900

    black formatting

--- video_annotator.py
+++ video_annotator.py
@@ -9,8 +9,7 @@ import cv2
 from PIL import Image, ImageTk
 
 
-class MainWindow():
-
+class MainWindow:
     def __init__(self, root, config_path):
 
         config = json.load(open(config_path, "r"))
@@ -24,10 +23,10 @@ class MainWindow():
         self.button_class = []
         self.video_width = config["video_display_width"]
         self.video_height = config["video_display_height"]
-        self.source_list = glob.glob(os.path.join(self.source_dir, '*'))
+        self.source_list = glob.glob(os.path.join(self.source_dir, "*"))
         self.source_num = len(self.source_list)
         self.json_path = config["json_path"]
-        self.interval = config["frame_display_interval"] # ms
+        self.interval = config["frame_display_interval"]  # ms
         self.loop_job_id = None
         self.init_window()
         self.set_video()
@@ -38,23 +37,39 @@ class MainWindow():
         self.root.title(u"Video Annotator")
 
         # display video
-        self.canvas = tkinter.Canvas(self.root,width=self.video_width,height=self.video_height)
-        self.canvas.grid(row=0, column=0,columnspan=6, rowspan=1)
+        self.canvas = tkinter.Canvas(
+            self.root, width=self.video_width, height=self.video_height
+        )
+        self.canvas.grid(row=0, column=0, columnspan=6, rowspan=1)
 
         # Buttons to change video
         self.button_next = tkinter.Button(
-            self.root, text="Next (→)", command=self.on_next_button, height=3)
-        self.button_next.grid(row=2, column=self.class_num+1, pady=10, sticky='nsew')
+            self.root, text="Next (→)", command=self.on_next_button, height=3
+        )
+        self.button_next.grid(row=2, column=self.class_num + 1, pady=10, sticky="nsew")
         self.button_back = tkinter.Button(
-            self.root, text="Back (←)", command=self.on_back_button, height=3)
-        self.button_back.grid(row=2, column=0, pady=10, sticky='nsew')
+            self.root, text="Back (←)", command=self.on_back_button, height=3
+        )
+        self.button_back.grid(row=2, column=0, pady=10, sticky="nsew")
 
         # Buttons for class labeling
         self.button_class = []
         for i, c in enumerate(self.class_list):
-            self.button_class.append(tkinter.Button(self.root, text="{}".format(c), command=lambda x=i:[self.labeling(class_num=x), self.on_next_button()], width=10, height=3))
-            self.button_class[i].grid(row=3, column=i+1, padx=5, pady=10, sticky='nsew')
-
+            self.button_class.append(
+                tkinter.Button(
+                    self.root,
+                    text="{}".format(c),
+                    command=lambda x=i: [
+                        self.labeling(class_num=x),
+                        self.on_next_button(),
+                    ],
+                    width=10,
+                    height=3,
+                )
+            )
+            self.button_class[i].grid(
+                row=3, column=i + 1, padx=5, pady=10, sticky="nsew"
+            )
 
     def set_video(self):
         if self.loop_job_id:
@@ -65,15 +80,22 @@ class MainWindow():
         # Set label
         self.selected_video_class = tkinter.StringVar()
         self.set_label()
-        self.label_image_class = tkinter.Label(self.root, textvariable=self.selected_video_class, width=self.class_num*10, background='#CCDDDD')
+        self.label_image_class = tkinter.Label(
+            self.root,
+            textvariable=self.selected_video_class,
+            width=self.class_num * 10,
+            background="#CCDDDD",
+        )
         self.label_image_class.grid(row=1, columnspan=7)
 
     def update_image(self):
         ret, image = self.cap.read()
         if ret:
-            self.image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # to RGB
-            self.image = Image.fromarray(self.image).resize((self.video_width, self.video_height)) # to PIL format
-            self.image = ImageTk.PhotoImage(self.image) # to ImageTk format
+            self.image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # to RGB
+            self.image = Image.fromarray(self.image).resize(
+                (self.video_width, self.video_height)
+            )  # to PIL format
+            self.image = ImageTk.PhotoImage(self.image)  # to ImageTk format
             # Update image
             self.canvas.create_image(0, 0, anchor=tkinter.NW, image=self.image)
             # Repeat every 'interval' ms
@@ -82,7 +104,9 @@ class MainWindow():
             self.set_video()
 
     def set_label(self):
-        self.selected_video_class.set("{}".format(self.get_class_name(self.source_list[self.current_video_num])))
+        self.selected_video_class.set(
+            "{}".format(self.get_class_name(self.source_list[self.current_video_num]))
+        )
 
     def get_class_name(self, video_path):
         data = self.load_json()
@@ -91,14 +115,14 @@ class MainWindow():
         else:
             return "No Label"
 
-    def on_next_button(self,e=None):
+    def on_next_button(self, e=None):
         self.current_video_num += 1
         # If last video, return to first video
         if self.current_video_num == self.source_num:
             self.current_video_num = 0
         self.set_video()
 
-    def on_back_button(self,e=None):
+    def on_back_button(self, e=None):
         self.current_video_num -= 1
         # If first video, return to last video
         if self.current_video_num == -1:
@@ -106,31 +130,32 @@ class MainWindow():
         self.set_video()
 
     def labeling(self, class_num):
-        video_path =self.source_list[self.current_video_num]
+        video_path = self.source_list[self.current_video_num]
         self.update_json(video_path, class_num)
         return
 
     def load_json(self):
         data = {}
         try:
-            data = json.load(open(self.json_path,'r'))
+            data = json.load(open(self.json_path, "r"))
         except json.JSONDecodeError as e:
             pass
         except FileNotFoundError as e:
-            with open(self.json_path, 'w'):
+            with open(self.json_path, "w"):
                 pass
         return data
 
     def update_json(self, video_path, class_num):
         data = self.load_json()
         data[video_path] = class_num
-        json.dump(data, open(self.json_path,'w'), indent=4)
+        json.dump(data, open(self.json_path, "w"), indent=4)
+
 
 @click.command()
 @click.option("--config_path", type=str, default="config/config.json")
 def main(config_path):
     root = tkinter.Tk()
-    MainWindow(root,config_path)
+    MainWindow(root, config_path)
     root.mainloop()
 
 
